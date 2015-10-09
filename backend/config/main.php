87a6f8183ef76436c4027ec4b54515d5f508b559
RubenDjOn <ruben@indahouser.com>
2015-10-09 03:09:50 +0200

    Add multiple packages
    Added following packages:
    
     - Giiant: Enhanced Gii
     - Translate manager: Allows manage translations via web
     - AdminLTE Theme: Good Theme for the backend
    
    First configuration of Translate manager.
    Basic integration of AdminLTE.

--- backend/config/main.php
+++ backend/config/main.php
@@ -10,8 +10,16 @@ return [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => ['log'],
-    'modules' => [],
+    'bootstrap' => ['log', 'translatemanager'],
+    'language' => 'en-US',
+    'modules' => [
+        'translatemanager' => [
+            'class' => 'lajax\translatemanager\Module',
+            'layout' => null,         // Name of the used layout. If using own layout use 'null'.
+            'ignoredCategories' => ['yii'], // these categories won’t be included in the language database.
+            'ignoredItems' => ['config'],   // these files will not be processed.
+        ],
+    ],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
@@ -28,6 +36,29 @@ return [
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
+        ],
+        'translatemanager' => [
+            'class' => 'lajax\translatemanager\Component',
+        ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\DbMessageSource',
+                    'db' => 'db',
+                    'sourceLanguage' => 'en-US', // Developer language
+                    'sourceMessageTable' => '{{%language_source}}',
+                    'messageTable' => '{{%language_translate}}',
+                    'cachingDuration' => 86400,
+                    'enableCaching' => true,
+                ],
+            ],
+        ],
+        'assetManager' => [
+            'bundles' => [
+                'dmstr\web\AdminLteAsset' => [
+                    'skin' => 'skin-red-light',
+                ],
+            ],
         ],
     ],
     'params' => $params,
