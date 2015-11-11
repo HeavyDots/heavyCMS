9bc082ab92aee89f938e0acd1e84f7a01a3365e3
RubenDjOn <ruben@indahouser.com>
2015-11-11 17:50:44 +0100

    Correct URLs examples

--- common/config/params.php
+++ common/config/params.php
@@ -6,6 +6,6 @@ return [
     'uploadDirectoryForURL' => 'uploads/',
     'frontendUploadDirectory' => Yii::getAlias('@frontend').'/web/uploads/',
     'backendUploadDirectory' => Yii::getAlias('@backend').'/web/uploads/',
-    'frontendURL' => 'overwrite on params-local.php. i.e. http://heavycms.com',
-    'backendURL' => 'overwrite on params-local.php. i.e. http://backend.heavycms.com',
+    'frontendURL' => 'overwrite on params-local.php. i.e. http://heavycms.com/',
+    'backendURL' => 'overwrite on params-local.php. i.e. http://backend.heavycms.com/',
 ];
